f12be654b19baa15c8381328af1f5e9493852152
miha <gomihagle@gmail.com>
2023-01-02 18:56:49 +0200

    mod

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ const express = require('express')
 const path = require('path')
 const favicon = require('serve-favicon')
 const fs = require('fs')
+const httpsRedirect = require('express-https-redirect')
 
 
+
+HTTP_PORT = 3000
 HTTPS_PORT = 3001
 
 const app = express()
 
-
+app.use(httpsRedirect())
 app.use(favicon('./client/favicon.ico'))
 app.use(express.static('client'))
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')))
@@ -21,6 +24,8 @@ app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')))
 function start() {
 
     try {
+        http.createServer(app).listen(HTTP_PORT, () => { console.log(`Server listening port ${HTTP_PORT}`) })
+
         https.createServer({
             
             key: fs.readFileSync('./sslcert/privkey.pem'),
